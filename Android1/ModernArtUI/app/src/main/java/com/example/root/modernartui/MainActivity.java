5968b4f528f46f4d4774e94f0109a579686cf3b9
cecy <cecilia@info-sync.com>
2015-03-23 16:24:10 -0600

    fibonacci rectangles

--- Android1/ModernArtUI/app/src/main/java/com/example/root/modernartui/MainActivity.java
+++ Android1/ModernArtUI/app/src/main/java/com/example/root/modernartui/MainActivity.java
@@ -1,17 +1,116 @@
 package com.example.root.modernartui;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import java.util.Arrays;
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
+    private CanvasView canvas;
+    int[] all_fibonacci = {89,55,34,21,13, 8, 5, 3, 2, 1, 1};
+    int[] sub_fibonacci;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout canvasLayout = (LinearLayout)findViewById(R.id.canvas);
+        canvas = new CanvasView(this);
+        canvasLayout.addView(canvas);
+
+    }
+
+
+    class CanvasView extends View {
+        int heightMax;
+        int lenSquare;
+        Canvas mCanvas;
+
+        public CanvasView(Context context) {
+            super(context);
+        }
+
+        protected void onDraw(Canvas canvas) {
+            mCanvas = canvas;
+            heightMax = canvas.getHeight();
+            sub_fibonacci = Arrays.copyOfRange(all_fibonacci, 0, 6);
+            calculatePoints();
+        }
+
+        protected void calculatePoints(){
+            lenSquare = heightMax/all_fibonacci[0];
+            Paint paint =new Paint();
+            paint.setStyle(Paint.Style.FILL);
+            String nextDirection = null;
+            int past_x_origin = 0;
+            int past_y_origin = 0;
+            int past_len = 0;
+            int x_origin = 0;
+            int y_origin = 0;
+            int lenRectangle;
+            for (int item : all_fibonacci) {
+                Random rnd = new Random();
+                paint.setARGB(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                System.out.println("Count is: " + item);
+                lenRectangle = lenSquare * item;
+                if (all_fibonacci[0] == item){
+                    x_origin = 0;
+                    y_origin = 0;
+                    mCanvas.drawRect(x_origin, y_origin, lenRectangle, lenRectangle, paint);
+                    nextDirection = "right";
+                }
+                else {
+                    if (nextDirection.equals("right")){
+                        nextDirection = "bottom";
+                        x_origin = past_x_origin+past_len;
+                        y_origin = past_y_origin;
+                        mCanvas.drawRect(x_origin, y_origin,
+                                    (past_len+past_x_origin)+(lenRectangle),
+                                    (lenRectangle)+(past_y_origin), paint);
+                    }
+                    else if(nextDirection.equals("bottom")){
+                        nextDirection = "left";
+                        x_origin = past_x_origin+past_len-lenRectangle;
+                        y_origin = past_y_origin+past_len;
+                        mCanvas.drawRect(x_origin, y_origin,
+                                past_x_origin+past_len,
+                                past_y_origin+past_len+(lenRectangle), paint);
+                    }
+                    else if(nextDirection.equals("left")){
+                        nextDirection = "up";
+                        x_origin = past_x_origin-lenRectangle;
+                        y_origin =  past_y_origin+past_len-lenRectangle;
+                        mCanvas.drawRect(x_origin, y_origin,
+                                past_x_origin,
+                                past_y_origin+past_len, paint);
+                    }
+                    else if(nextDirection.equals("up")) {
+                        nextDirection = "right";
+                        x_origin = past_x_origin;
+                        y_origin =  past_y_origin-(lenRectangle);
+                        mCanvas.drawRect(x_origin, y_origin,
+                                past_x_origin+(lenRectangle),
+                                past_y_origin, paint);
+                    }
+                }
+                past_x_origin = x_origin;
+                past_y_origin = y_origin;
+                past_len = lenRectangle;
+            }
+
+        }
+
     }
 
 
