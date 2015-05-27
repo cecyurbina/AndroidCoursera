215d3f6d7e71e556b454772b3433b0e71d190486
cecy <cecilia@info-sync.com>
2015-05-26 23:29:25 -0500

    assignment 2 communication (messages, services)

---
+++ Android4/assignment2/app/src/main/java/vandy/mooc/utils/ReplyMessage.java
@@ -0,0 +1,80 @@
+package vandy.mooc.utils;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Message;
+
+/**
+ * A thin facade around an Android Message that defines the schema of
+ * a reply from the Service back to the Activity.
+ */
+public class ReplyMessage extends RequestReplyMessageBase {
+    /**
+     * Constructor is private to ensure the makeReplyMessage() factory
+     * method is used.
+     */
+    private ReplyMessage(Message message) {
+        super(message);
+    }
+
+    /**
+     * Convert a Message into a ReplyMessage.
+     */
+    public static ReplyMessage makeReplyMessage(Message message) {
+        // Make a copy of @a message since it may be recycled.
+        return new ReplyMessage(Message.obtain(message));
+    }
+
+    /**
+     * A factory method that creates a reply message to return to the
+     * Activity with the pathname of the downloaded image.
+     */
+    public static ReplyMessage makeReplyMessage(Uri pathToImageFile,
+                                                Uri url,
+                                                int requestCode) {
+        // Create a ReplyMessage that holds a reference to a Message
+        // created via the Message.obtain() factory method.
+        ReplyMessage replyMessage =
+            new ReplyMessage(Message.obtain());
+
+        // Create a new Bundle to handle the result.
+        // TOD -- you fill in here.
+        Bundle b = new Bundle();
+
+        // Set the Bundle to be the data in the message.
+        // TOD -- you fill in here.
+        replyMessage.setData(b);
+
+
+        // Put the URL to the image file into the Bundle
+        // TOD -- you fill in here.
+        b.putString(IMAGE_URL, url.toString());
+
+
+        // Put the requestCode into the Bundle
+        // TOD -- you fill in here.
+        b.putInt(REQUEST_CODE, requestCode);
+
+        if(pathToImageFile != null) {
+            // Set the result code to indicate whether the download
+            // succeeded or failed.
+            // TOD -- you fill in here.
+
+            replyMessage.getMessage().arg1 = Activity.RESULT_OK;
+            // Put the path to the image file into the Bundle
+            // only if the download succeeded.
+            // TOD -- you fill in here.
+            b.putString(IMAGE_PATHNAME, pathToImageFile.toString());
+        }else{
+            replyMessage.getMessage().arg1 = Activity.RESULT_CANCELED;
+        }
+
+
+
+
+
+
+        return replyMessage;
+    }
+}
